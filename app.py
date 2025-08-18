ae28cd6dffd58bc3d1b3e1893f52b71332d7dc8d
23f2000275 <23f2000275@ds.study.iitm.ac.in>
2025-08-18 08:03:37 +0530

    Update 4 app.py

--- app.py
+++ app.py
@@ -280,6 +280,6 @@ async def upload_files(files: List[UploadFile] = File(...)):
 #     import uvicorn
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
 import uvicorn
-port = int(os.environ.get("PORT", 8000))
+# port = int(os.environ.get("PORT", 8000))
 
-uvicorn.run(app, host="0.0.0.0", port=port)
+# uvicorn.run(app, host="0.0.0.0", port=port)
