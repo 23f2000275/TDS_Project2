9cc721e543d23376e503055ebec0e0d76946d5fa
23f2000275 <23f2000275@ds.study.iitm.ac.in>
2025-08-18 07:28:58 +0530

    Update 2 app.py

--- app.py
+++ app.py
@@ -276,6 +276,9 @@ async def upload_files(files: List[UploadFile] = File(...)):
     return run_result["output"], run_result['error'], run_result['code']
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+port = int(os.environ.get("PORT", 8000))
+
+uvicorn.run(app, host="0.0.0.0", port=port)
